5aae9b0d65dc3710ce17aa278805171993f65d23
xiangzhemeng <xiangzhemeng@tsf-444-wpa-1-116.epfl.ch>
2017-12-11 18:39:25 +0100

    update preprocessing cpu file

--- preprocessing_cpu.py
+++ preprocessing_cpu.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import enchant
 
 dict = {}
 corpus = open('tweet_typo_corpus.txt', 'rb')
@@ -7,6 +8,17 @@ for word in corpus:
     word = word.split()
     dict[word[1]] = word[3]
 corpus.close()
+
+def remove_repetitions(tweet):
+    dict_us = enchant.Dict('en_US')
+    tweet=tweet.split()
+    for i in range(len(tweet)):
+        tweet[i]=''.join(''.join(s)[:2] for _, s in itertools.groupby(tweet[i])).replace('#', '')
+        if len(tweet[i])>0:
+            if not dict_us.check(tweet[i]):
+                tweet[i] = ''.join(''.join(s)[:1] for _, s in itertools.groupby(tweet[i])).replace('#', '')
+    tweet = ' '.join(tweet)
+    return tweet
 
 def spelling_correction(tweet):
     tweet = tweet.split()
@@ -17,15 +29,16 @@ def spelling_correction(tweet):
     return tweet
 
 def clean(tweet):
+    tweet = remove_repetitions(tweet)
     tweet = spelling_correction(tweet)
     return tweet.strip().lower()
 
 X_test = pd.read_pickle("test_tweets_after_preprocess.pkl")
 X_test['tweet'] = X_test['tweet'].apply(lambda tweet: clean(tweet))
-X_test.to_pickle("test_tweets_after_preprocess_spelling_corr.pkl")
+X_test.to_pickle("test_tweets_after_preprocess_new.pkl")
 print("test preprocessing finished!")
 
 X_train = pd.read_pickle("train_tweets_after_preprocess_cnn.pkl")
 X_train['tweet'] = X_train['tweet'].apply(lambda tweet: clean(tweet))
-X_train.to_pickle("train_tweets_after_preprocess_cnn_spelling_corr.pkl")
+X_train.to_pickle("train_tweets_after_preprocess_cnn_new.pkl")
 print("train preprocessing finished!")
