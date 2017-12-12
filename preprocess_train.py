ea0a00b0c568e2032db1438e672ce6093698c8a5
xiangzhemeng <xiangzhemeng@tsf-444-wpa-1-116.epfl.ch>
2017-12-12 10:12:40 +0100

    update preprocess

--- preprocess_train.py
+++ preprocess_train.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import numpy as np
 import itertools
 import enchant
 import multiprocessing
+from multiprocessing import Pool
 
-multiprocessing.cpu_count()
+num_partitions = 20
+num_cores = multiprocessing.cpu_count()
 
 dict = {}
 corpus = open('tweet_typo_corpus.txt', 'rb')
@@ -37,7 +40,19 @@ def clean(tweet):
     tweet = spelling_correction(tweet)
     return tweet.strip().lower()
 
+def parallelize_dataframe(df, func):
+    df_split = np.array_split(df, num_partitions)
+    pool = Pool(num_cores)
+    df = pd.concat(pool.map(func, df_split))
+    pool.close()
+    pool.join()
+    return df
+
+def multiply_columns(data):
+    data['tweet'] = data['tweet'].apply(lambda x: clean(x))
+    return data
+
 X_train = pd.read_pickle("train_tweets_after_preprocess_cnn.pkl")
-X_train['tweet'] = X_train['tweet'].apply(lambda tweet: clean(tweet))
+X_train = parallelize_dataframe(X_train, multiply_columns)
 X_train.to_pickle("train_tweets_after_preprocess_cnn_new.pkl")
 print("train preprocessing finished!")
