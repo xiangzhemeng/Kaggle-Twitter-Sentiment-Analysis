e6f3b45b98ed38c3439b22f5651910e43d40e1d9
xiangzhemeng <xiangzhemeng@tsf-444-wpa-7-211.epfl.ch>
2017-12-16 11:54:36 +0100

    update run2

--- run.py
+++ run.py
@@ -3,19 +3,15 @@ import pickle
 import csv
 import xgboost as xgb
 
-train1 = pickle.load(open("train_model1.txt", "rb"))
-train2 = pickle.load(open("train_model2.txt", "rb"))
-train3 = pickle.load(open("train_model3.txt", "rb"))
-train4 = pickle.load(open("train_model4.txt", "rb"))
+train1 = pickle.load(open("train/train_model1.txt", "rb"))
+train4 = pickle.load(open("train/train_model4.txt", "rb"))
 
-test1 = pickle.load(open("test_model1.txt", "rb"))
-test2 = pickle.load(open("test_model2.txt", "rb"))
-test3 = pickle.load(open("test_model3.txt", "rb"))
-test4 = pickle.load(open("test_model4.txt", "rb"))
+test1 = pickle.load(open("test/test_model1.txt", "rb"))
+test4 = pickle.load(open("test/test_model4.txt", "rb"))
 
 
-train = np.hstack((train1, train2, train3, train4))
-test = np.hstack((test1, test2, test3, test4))
+train = np.hstack((train1, train4))
+test = np.hstack((test1, test4))
 y = np.array(1250000 * [0] + 1250000 * [1])
 np.random.seed(0)
 np.random.shuffle(y)
@@ -26,7 +22,7 @@ model = xgb.XGBClassifier().fit(train, y)
 y_pred = model.predict(test)
 y_pred = 1 - 2 * y_pred
 
-with open('cnn_submission5.csv', 'w') as file:
+with open('run_submission9.csv', 'w') as file:
     fieldnames = ['Id', 'Prediction']
     writer = csv.DictWriter(file, delimiter=",", fieldnames=fieldnames)
     writer.writeheader()
