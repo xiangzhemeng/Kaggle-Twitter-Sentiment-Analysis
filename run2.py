558a06d4b9007a569391b2ad678010cb39ff601f
xiangzhemeng <xiangzhemeng@tsf-444-wpa-7-211.epfl.ch>
2017-12-16 11:53:01 +0100

    update run2

--- run2.py
+++ run2.py
@@ -4,16 +4,14 @@ import csv
 import xgboost as xgb
 
 train1 = pickle.load(open("train_model1_2.txt", "rb"))
-train3 = pickle.load(open("train_model3_2.txt", "rb"))
 train4 = pickle.load(open("train_model4_2.txt", "rb"))
 
 test1 = pickle.load(open("test_model1_2.txt", "rb"))
-test3 = pickle.load(open("test_model3_2.txt", "rb"))
 test4 = pickle.load(open("test_model4_2.txt", "rb"))
 
 
-train = np.hstack((train1, train3, train4))
-test = np.hstack((test1, test3, test4))
+train = np.hstack((train1, train4))
+test = np.hstack((test1, test4))
 y = np.array(int(2500000/2) * [0] + int(2500000/2) * [1])
 np.random.seed(0)
 np.random.shuffle(y)
@@ -24,7 +22,7 @@ model = xgb.XGBClassifier().fit(train, y)
 y_pred = model.predict(test)
 y_pred = 1 - 2 * y_pred
 
-with open('run_submission7.csv', 'w') as file:
+with open('run_submission8.csv', 'w') as file:
     fieldnames = ['Id', 'Prediction']
     writer = csv.DictWriter(file, delimiter=",", fieldnames=fieldnames)
     writer.writeheader()
